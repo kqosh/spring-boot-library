11956fecccf0f4267c1243a661daec9876a3d65c
Ronald <ronald@ronaldkoster.net>
2025-07-03 13:42:13 +0200

    v20250703d

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO book (id, isbn, author_name, title, publisher)
-VALUES (101, 'isbn123   ', 'Jan Klaassen', 'De poppenkast', 'De Uitgeverij');
+VALUES (101, 'isbn123', 'Jan Klaassen', 'De poppenkast', 'De Uitgeverij');

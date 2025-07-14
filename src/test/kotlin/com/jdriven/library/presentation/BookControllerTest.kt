a8ffa817a0fc0f67b02fb74843c79a6d3539deea
Ronald <ronald@ronaldkoster.net>
2025-07-14 13:43:23 +0200

    upd-0714b

--- src/test/kotlin/com/jdriven/library/presentation/BookControllerTest.kt
+++ src/test/kotlin/com/jdriven/library/presentation/BookControllerTest.kt
@@ -1,7 +1,6 @@
 package com.jdriven.library.presentation
 
 import com.jdriven.library.service.model.Book
-import com.jdriven.library.service.model.Checkout
 import io.restassured.RestAssured
 import io.restassured.common.mapper.TypeRef
 import io.restassured.response.ResponseBodyExtractionOptions
@@ -43,26 +42,43 @@ class BookControllerTest() {
 
 	@Test
 	fun search_byAuthor() {
-		val books = search("RENE", null, 200)
+		val books = searchAsBooks("RENE", null, 200)
 		Assertions.assertEquals(1, books.size)
 		Assertions.assertEquals("Rene Goscinny", books[0].authorName)
 	}
 
 	@Test
+	fun search_byAuthorNotFound() {
+		val books = searchAsBooks("HARRY", null, 200)
+		Assertions.assertEquals(0, books.size)
+//		Assertions.assertEquals("Rene Goscinny", books[0].authorName)
+	}
+
+	@Test
+	fun search_NoArgs() {
+		val books = searchAsRspOptions("", null, 400)
+//		Assertions.assertEquals(0, books.size)
+////		Assertions.assertEquals("Rene Goscinny", books[0].authorName)
+	}
+
+	@Test
 	fun search_byTitle() {
-		val books = search(null, "DE POP", 200)
+		val books = searchAsBooks(null, "DE POP", 200)
 		Assertions.assertEquals(3, books.size)
 		books.forEach { Assertions.assertTrue(it.title!!.startsWith("De poppenkast")) }
 	}
 
 	@Test
 	fun search_byAuthorAndTitle() {
-		val books = search("jan", "de poppenkast", 200)
+		val books = searchAsBooks("jan", "de poppenkast", 200)
 		Assertions.assertEquals(2, books.size)
 		books.forEach { Assertions.assertTrue(it.title!!.startsWith("De poppenkast")) }
 	}
 
-	private fun search(author: String?, title: String?, expectedStatusCode: Int): List<Book> {
+	private fun searchAsBooks(author: String?, title: String?, expectedStatusCode: Int): List<Book> =
+		searchAsRspOptions(author, title, expectedStatusCode).`as`(object : TypeRef<List<Book>>() {})
+
+	private fun searchAsRspOptions(author: String?, title: String?, expectedStatusCode: Int): ResponseBodyExtractionOptions {
 		var url = "http://localhost:${port}/books/search"
 		var separator = "?"
 		if (author != null) {
@@ -70,27 +86,23 @@ class BookControllerTest() {
 			separator = "&"
 		}
 		if (title != null) url += "${separator}title=${title}"
-		return RestCallBuilder(url, expectedStatusCode).username("user101").password("pwuser").get().`as`(object : TypeRef<List<Book>>() {})
+		return RestCallBuilder(url, expectedStatusCode).username("user101").password("pwuser").get()
 	}
-	//qqqq test create, delete
-	/*qqqq
-
-	@Test
-	fun createFindDelete() {
-		val baseUrl = "http://localhost:${port}/authors"
-		val name = "Klaas"
-		run {
-			RestCallBuilder(baseUrl, 201).body(CreateAuthorRequest(name)).username("admin").password("pwadmin").post()
-		}
-		run {
-			findByName(name, 200)
-		}
-		run {
-			RestCallBuilder("${baseUrl}/${name}", 200).username("admin").password("pwadmin").delete()
-
-			findByName(name, 404)
-		}
-	}
-
-	 */
+//
+//	@Test
+//	fun createFindDelete() {
+//		val baseUrl = "http://localhost:${port}/authors"
+//		val name = "Klaas"
+//		run {
+//			RestCallBuilder(baseUrl, 201).body(CreateAuthorRequest(name)).username("admin").password("pwadmin").post()
+//		}
+//		run {
+//			findByName(name, 200)
+//		}
+//		run {
+//			RestCallBuilder("${baseUrl}/${name}", 200).username("admin").password("pwadmin").delete()
+//
+//			findByName(name, 404)
+//		}
+//	}qqqq
 }
